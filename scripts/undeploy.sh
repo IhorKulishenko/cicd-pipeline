3355b0eb4eeff818a4192886968dbcaddd47f5e0
IhorK <ihor.kulishenko@gmail.com>
2025-03-05 20:59:33 +0200

    added debugin to script

--- scripts/undeploy.sh
+++ scripts/undeploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -x 
+
 container_name=$1
 
-container_id=$(docker ps --filter "name=$container_name"  --format='{{json .ID}}')
+container_id=$(docker ps --filter "name=${container_name}"  --format='{{json .ID}}')
 
-if [[ -n "$container_id" ]]; then
-    docker container stop $container_id
-    docker container rm $container_id
+if [[ -n "${container_id}" ]]; then
+    docker container stop "${container_id}"
+    docker container rm "${container_id}"
 fi
+
+set +x 
