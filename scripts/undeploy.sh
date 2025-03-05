6eab4b5fb872a2b7b1e681817da46e26b4770217
IhorK <ihor.kulishenko@gmail.com>
2025-03-05 21:44:34 +0200

    fixed script

--- scripts/undeploy.sh
+++ scripts/undeploy.sh
@@ -4,7 +4,7 @@ set -x
 
 container_name=$1
 
-container_id=$(docker ps --filter "name=${container_name}"  --format='{{json .ID}}')
+container_id=$(docker ps --filter "name=${container_name}"  --format='{{json .ID}}' | tr -d '"')
 
 if [[ -n "${container_id}" ]]; then
     docker container stop ${container_id}
